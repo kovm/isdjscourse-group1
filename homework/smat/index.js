0381c5f5970275281b723b87c0946891756cfb79
stasdsgn <stas.dsgn@gmail.com>
2019-12-12 13:43:59 +0200

    Iterator and Palindrome Checker

--- homework/smat/index.js
+++ homework/smat/index.js
@@ -1,49 +1,47 @@
-let truesum = 0;
-let truesumdiv = document.querySelector('.trueres');
-let falsesum = 0;
-let falsesumdiv = document.querySelector('.falseres');
-
-document.querySelector('#choose').addEventListener('click', runRes);
-generator();
-
-function generator() {
-numb1 = Math.ceil(Math.random()*3);
-let lis = document.getElementsByTagName('h1');
-val1 = Math.ceil(Math.random()*8);
-val2 = Math.ceil(Math.random()*9);
-val = val1 * val2; 
-lis[0].textContent = `${val1} x ${val2} = ?`;
-let numbers = [];
-while (numbers.length < 4) {
-    rndnumber = Math.ceil(Math.random() * 99);
-    if(rndnumber !== val){
-        if (numbers.indexOf(rndnumber) == -1) {     
-            numbers.push(rndnumber);  
-        }
-    }
- }
- 
-const items = document.getElementsByClassName('box');
-
-for(let i = 0; i < 4; i++) {
-    if(i !== numb1){ 
-        items[i].textContent = numbers[i];
-    } else { 
-           items[i].textContent = val;
-           items[i].classList.add('answer');
-    }
-}
-}
-
-function runRes(e){
-    if(e.target.classList.contains('answer')){
-        truesum++;
-        truesumdiv.textContent = `Верных ответов: ${truesum}`;
-    }
-    else {
-        falsesum++;
-        falsesumdiv.textContent = `Неверных ответов: ${falsesum}`;
-    }
-    document.querySelector('.answer').classList.remove('answer');
-    generator();
-}
+const counterFB = (function () {
+   const a = 100;
+   const dividers = {
+      Fizz: 3,
+      Buzz: 5
+   };
+   const logs = Object.keys(dividers);
+   function counterLog () {
+      for (let i = 1; i <= a; i++) {
+         if (!(i % dividers.Fizz)) {
+            if (!(i % dividers.Buzz)) {
+               console.log(`${logs[0]}${logs[1]}`);
+            } else {
+               console.log(`${logs[0]}`);
+            }
+         } else {
+            if (!(i % dividers.Buzz)) {
+               console.log(`${logs[1]}`);
+            } else {
+               console.log(i);
+            }
+         }
+      }
+   }
+   return { logging: counterLog };
+})();
+counterFB.logging();
+const test = 'Was it a cat I saw?';
+const isPalindrome = (function () {
+   const regExp = /[^A-Za-zА-Яа-яЁёІіЇї0-9]/g;
+   const newtest = test.replace(regExp, '').toLowerCase();
+   const testLoop = Math.floor(newtest.length / 2);
+   function charTest () {
+      for (let i = 1; i <= testLoop; i++) {
+         if (newtest[i - 1] !== newtest[newtest.length - i]) {
+            console.log(`${test} - isn't a palindrome`);
+            break;
+         } else {
+            if (i === testLoop) {
+               console.log(`${test} - is a palindrome`);
+            }
+         }
+      }
+   }
+   return { isPalindrome: charTest };
+})();
+isPalindrome.isPalindrome();
