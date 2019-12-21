994f7ff55ebd7848e923711a555582b0a34d9867
Ilia <ilia.frolov@simedis.com>
2019-12-21 21:54:41 +0200

    Redone without forking

--- homework/froi/index.js
+++ homework/froi/index.js
@@ -1,1 +1,42 @@
-console.log(`Hello World!`);
+function FizzBuzz () {
+   const numbers = [];
+   for (let i = 1; i <= 100; i++) {
+      let n = i;
+      switch (true) {
+      case (n % 3 === 0 && n % 5 === 0):
+         n = 'FizzBuzz';
+         break;
+      case (n % 3 === 0):
+         n = 'Fizz';
+         break;
+      case (n % 5 === 0):
+         n = 'Buzz';
+         break;
+      default:
+      }
+      numbers.push(n);
+   }
+   console.log(numbers.join(', '));
+   document.body.append(numbers.join(', '));
+}
+FizzBuzz();
+
+function isPolindrome (subject) {
+   if (typeof subject !== 'number' || typeof subject !== 'string' || subject === undefined) {
+      return false;
+   }
+   return subject.toLowerCase()
+      .split('').filter(element => element !== ' ')
+      .every((element, index, arr) => element === arr[arr.length - index - 1]);
+}
+
+const subject1 = 'кот';
+const subject2 = 'потоп';
+const subject3 = '12311';
+const subject4 = '345543';
+const subject5 = 'Аргентина манит негра';
+console.log(isPolindrome(subject1));
+console.log(isPolindrome(subject2));
+console.log(isPolindrome(subject3));
+console.log(isPolindrome(subject4));
+console.log(isPolindrome(subject5));
