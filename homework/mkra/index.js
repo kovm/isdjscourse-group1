803d647306c3165be04ed4e6e0481d4ff1f21fef
Max0293 <emaxkravchenko@gmail.com>
2019-12-26 14:19:51 +0200

    Comments fixed

--- homework/mkra/index.js
+++ homework/mkra/index.js
@@ -4,17 +4,15 @@ function createSeaBattle () {
    const missFire = -1;
    const hitFire = 0;
    const deathHit = 1;
-   let battleField = createBattleField(10, 10);
    const usedCells = [];
-   battleField = createShips(battleField);
+   const battleField = createShips(createBattleField(10, 10));
 
-   return function parentFn (x) {
-      return function childFn (y) {
+   return (x) => {
+      return (y) => {
          validate(x, y, usedCells);
 
          if (checkEndGame(battleField)) {
-            // eslint-disable-next-line no-throw-literal
-            throw 'Game over';
+            throw new Error('Game over');
          } else if (battleField[x][y] === true) {
             battleField[x][y] = false;
             usedCells.push([x, y]);
@@ -60,16 +58,14 @@ function createShips (array) {
 }
 
 function checkEndGame (array) {
-   let result = true;
    for (const rows of array) {
       for (const cell of rows) {
          if (cell === true) {
-            result = false;
-            return result;
+            return false;
          }
       }
    }
-   return result;
+   return true;
 }
 
 function createBattleField (rows, columns) {
@@ -87,10 +83,10 @@ function isShipDestroyed (array, x, y) {
    let result = true;
    let checkX = true;
    let checkY = true;
-   let leftKey = y - 1;
-   let rightKey = y + 1;
-   let upperKey = x + 1;
-   let bottomKey = x - 1;
+   let bottomKey = y - 1;
+   let upperKey = y + 1;
+   let rightKey = x + 1;
+   let leftKey = x - 1;
 
    while (array[upperKey][y] !== null && array[bottomKey][y] !== null) {
       if (array[upperKey][y] === true || array[bottomKey][y] === true) {
@@ -112,20 +108,16 @@ function isShipDestroyed (array, x, y) {
 }
 
 function validate (currentX, currentY, array) {
-   let result = false;
    let oldX;
    let oldY;
    if (currentX < 0 || currentX > 9) {
-      // eslint-disable-next-line no-throw-literal
-      throw 'You are enter incorrect number (correct = 0-9)';
+      throw new Error('You are enter incorrect number (correct = 0-9)');
    }
    if (currentY < 0 || currentY > 9) {
-      // eslint-disable-next-line no-throw-literal
-      throw 'You are enter incorrect number (correct = 0-9)';
+      throw new Error('You are enter incorrect number (correct = 0-9)');
    }
    if (typeof (currentX) !== 'number' || typeof (currentY) !== 'number') {
-      // eslint-disable-next-line no-throw-literal
-      throw 'You are enter incorrect data';
+      throw new Error('You are enter incorrect data');
    }
    for (const old of array) {
       for (let i = 0; i < 2; i++) {
@@ -135,12 +127,9 @@ function validate (currentX, currentY, array) {
             oldY = old[i];
          }
       }
-      result = oldX === currentX && oldY === currentY;
-      if (result) {
-         // eslint-disable-next-line no-throw-literal
-         throw 'You already shot in this cell';
+      if (oldX === currentX && oldY === currentY) {
+         throw new Error('You already shot in this cell');
       }
    }
 }
-
 let seaBattle = createSeaBattle();
