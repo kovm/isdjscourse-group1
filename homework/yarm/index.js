856a17e91c65c078dd2a739b9efd9399848ef739
Yaroslav <4cast@ukr.net>
2019-12-28 12:47:48 +0200

    start working on RiverBattle

--- homework/yarm/index.js
+++ homework/yarm/index.js
@@ -1,35 +1,66 @@
-// Palindrome
-
-function palindrome (str) {
-   if (typeof str === 'number' || typeof str === 'string') {
-      str = str.toString().toLowerCase().replace(/\s/g, '');
-   } else {
-      return console.log('Error input, please to correct check your request enter in string format example "your word" or just enter a number');
-   };
-
-   const arr = str.split('');
-   const arrLength = (arr.length % 2) ? ((arr.length - 1) / 2) : (arr.length / 2);
-   let textShift = '';
-   let textPop = '';
-
-   for (let i = 0; i < arrLength; i++) {
-      textShift += arr.shift();
-      textPop += arr.pop();
-   }
-   return textShift === textPop;
-}
-palindrome('ANna KA NNa');
-
-// FizzBuzz
-
-for (let i = 1; i <= 100; i++) {
-   if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz');
-   } else if (i % 3 === 0) {
-      console.log('Fizz');
-   } else if (i % 5 === 0) {
-      console.log('Buzz');
-   } else {
-      console.log(i);
-   }
-}
+function createSeaBattle () {
+   const seaMap = [
+      {
+         id: '01',
+         ship: true,
+         x: 0,
+         y: 0
+      },
+      {
+         id: '02',
+         ship: true,
+         x: 0,
+         y: 1
+      },
+      {
+         id: '03',
+         ship: false,
+         x: 0,
+         y: 2
+      },
+      {
+         id: '04',
+         ship: false,
+         x: 0,
+         y: 3
+      },
+      {
+         id: '05',
+         ship: false,
+         x: 0,
+         y: 4
+      },
+      {
+         id: '06',
+         ship: false,
+         x: 0,
+         y: 5
+      },
+      {
+         id: '07',
+         ship: true,
+         x: 0,
+         y: 6
+      },
+      {
+         id: '08',
+         ship: false,
+         x: 0,
+         y: 7
+      },
+      {
+         id: '09',
+         ship: true,
+         x: 0,
+         y: 8
+      },
+      {
+         id: '10',
+         ship: false,
+         x: 0,
+         y: 9
+      }
+   ];
+   console.log(seaMap);
+};
+createSeaBattle();
