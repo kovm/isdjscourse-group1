6d4cd4d42adad6e693df2417c09a2683f160a365
Yaroslav <4cast@ukr.net>
2019-12-28 23:26:58 +0200

    Add checking when shoting into the ship

--- homework/yarm/index.js
+++ homework/yarm/index.js
@@ -1,14 +1,14 @@
-function createSeaBattle () {
+function createSeaBattle (a, b) {
    const seaMap = [
       {
          id: '01',
-         ship: true,
+         ship: false,
          x: 0,
          y: 0
       },
       {
          id: '02',
-         ship: true,
+         ship: false,
          x: 0,
          y: 1
       },
@@ -20,7 +20,7 @@ function createSeaBattle () {
       },
       {
          id: '04',
-         ship: false,
+         ship: true,
          x: 0,
          y: 3
       },
@@ -44,13 +44,13 @@ function createSeaBattle () {
       },
       {
          id: '08',
-         ship: false,
+         ship: true,
          x: 0,
          y: 7
       },
       {
          id: '09',
-         ship: true,
+         ship: false,
          x: 0,
          y: 8
       },
@@ -61,6 +61,28 @@ function createSeaBattle () {
          y: 9
       }
    ];
-   console.log(seaMap);
+   const shotCount = [];
+   const shotX = seaMap[a].id;
+   function shots () {
+      shotCount.push(shotX);
+   }
+   shots();
+   console.log(shotCount);
+   function shipOne () {
+      if (seaMap[+shotX].ship === seaMap[6].ship || seaMap[+shotX].ship === seaMap[7].ship) {
+         return console.log('Shot!!!');
+      } else {
+         return console.log('NO ship crashed');
+      }
+   }
+   if (seaMap[a].ship === true) {
+      return shipOne();
+   } else {
+      console.log('U R miss!!!');
+   }
+
+   console.log(seaMap, shotX, seaMap.length, seaMap[0].ship);
 };
-createSeaBattle();
+createSeaBattle(3);
+createSeaBattle(6);
+createSeaBattle(0);
