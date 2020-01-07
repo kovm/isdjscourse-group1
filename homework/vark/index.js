927702cce9ca0564830450ea370fcce1cd1342dd
Konstantin Varlamov <vrlmvkst@gmail.com>
2020-01-07 18:16:30 +0200

    homework2 eslint check

--- homework/vark/index.js
+++ homework/vark/index.js
@@ -1,34 +1,27 @@
-var randomNumberTo50 = 1 + Math.floor(Math.random() * (50 + 1 - 1));
-var programNumber = randomNumberTo50;
-var promptToPlay = "Попробуй отгадать число от 1 до 50 "; 
-var userInput = prompt(promptToPlay);
-var userGuess = parseInt(userInput, 10);
-var previosGuess;
-var maxAttempts = 10;
-var currentAttepmtIndex;
-for (currentAttepmtIndex = 1; currentAttepmtIndex <= maxAttempts; currentAttepmtIndex += 1) {
-	//if (isNaN(userGuess)) {
-	//	alert("Only numbers");
-	//} else {
-	/*if(previosGuess === 0) {
-		previosGuess = userGuess;
-		userGuess = prompt('Enter the number');
-
-	} else*/
-	if (userGuess == programNumber) {
-		alert('You win! with ' + currentAttepmtIndex + 'attempt(s)');
-		break;
-
-	} else if (Math.abs(programNumber - userGuess) < Math.abs(programNumber - previosGuess)) {
-		previosGuess = userGuess;
-		userGuess = prompt('Hoter');
-
-	} else {
-		previosGuess = userGuess;
-		userGuess = prompt('Colder');
-	}
-	//}
-	if (currentAttepmtIndex === maxAttempts - 1) {
-		alert('you lose');
-	}
+for (let i = 1; i <= 100; i += 1) {
+   let output = '';
+   if (i % 3 === 0) {
+      output += 'Fizz';
+   }
+   if (i % 5 === 0) {
+      output += 'Bazz';
+   }
+   console.log(output || i);
 }
+
+const isPali = (input) => {
+   if (!input) {
+      return false;
+   }
+   let str = input.toString();
+   const strLength = str.length;
+   str = str.toLowerCase();
+   for (let i = 0; i < strLength; i += 1) {
+      if (str[i] !== str[strLength - 1 - i]) {
+         return false;
+      }
+   }
+   return true;
+};
+
+isPali(123321);
