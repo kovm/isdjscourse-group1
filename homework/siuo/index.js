7f7caf21ee44dc0da0496c49ae785f91a1c94358
Alex S <sasha.siundiukov@gmail.com>
2020-01-15 10:33:19 +0300

    Edits made

--- homework/siuo/index.js
+++ homework/siuo/index.js
@@ -1,16 +1,34 @@
+const count = 100;
+
+for (let i = 1; i <= count; i++) {
+   if (i % 5 === 0 && i % 3 === 0) {
+      console.log('FizzBuzz');
+   } else if (i % 5 === 0) {
+      console.log('Buzz');
+   } else if (i % 3 === 0) {
+      console.log('Fizz');
+   } else {
+      console.log(i);
+   }
+}
+
 function isPolindrome (str) {
+   if (typeof str !== 'string' && typeof str !== 'number') {
+      return console.log('Enter a number or a string more than 3 characters, please!');
+   } else if (!/\w{3,}/.test(str)) {
+      return console.log('Enter a number or a string more than 3 characters, please!');
+   }
    const someStr = str.toString();
    let i = 0;
    let j = someStr.length - 1;
-   let yesOrNo = true;
    for (let l = 0; l <= someStr.length / 2; l++) {
       if (someStr[i] !== someStr[j]) {
-         yesOrNo = false;
+         return `${someStr} isn't a palindrome!`;
       }
       i++;
       j--;
    }
-   return yesOrNo;
+   return `${someStr} is a palindrome!`;
 }
 
-console.log(isPolindrome('potop'));
+console.log(isPolindrome('1g '));
