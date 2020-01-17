7ab62e79cc03d37ebf9d9121cd21bf46b3b5b786
Oleksandr Siundiukov <56542879+sasha-si@users.noreply.github.com>
2020-01-17 10:31:27 +0200

    Added a homework2 (#39)
    * Added a homework2
    
    * Edits made

--- homework/siuo/index.js
+++ homework/siuo/index.js
@@ -1,17 +1,34 @@
-function bubbleSortConcept1(arr) {
+const count = 100;
 
-    for (let j = arr.length - 1; j > 0; j--) {
-      for (let i = 0; i < j; i++) {
-        if (arr[i] > arr[i + 1]) {
-          let temp = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = temp;
-        }
+for (let i = 1; i <= count; i++) {
+   if (i % 5 === 0 && i % 3 === 0) {
+      console.log('FizzBuzz');
+   } else if (i % 5 === 0) {
+      console.log('Buzz');
+   } else if (i % 3 === 0) {
+      console.log('Fizz');
+   } else {
+      console.log(i);
+   }
+}
+
+function isPolindrome (str) {
+   if (typeof str !== 'string' && typeof str !== 'number') {
+      return console.log('Enter a number or a string more than 3 characters, please!');
+   } else if (!/\w{3,}/.test(str)) {
+      return console.log('Enter a number or a string more than 3 characters, please!');
+   }
+   const someStr = str.toString();
+   let i = 0;
+   let j = someStr.length - 1;
+   for (let l = 0; l <= someStr.length / 2; l++) {
+      if (someStr[i] !== someStr[j]) {
+         return `${someStr} isn't a palindrome!`;
       }
-    }
-    return arr;
-  }
+      i++;
+      j--;
+   }
+   return `${someStr} is a palindrome!`;
+}
 
-  let unsortedArr = [5, 8, 3, 1, 2];
-
-console.log(bubbleSortConcept1(unsortedArr));
+console.log(isPolindrome('1g '));
