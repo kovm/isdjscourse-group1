e099126a592e064446358ae08000caf3ccf107a7
Yaroslav <4cast@ukr.net>
2020-01-25 17:56:18 +0200

    working under throwing correct errors

--- homework/yarm/index.js
+++ homework/yarm/index.js
@@ -8,12 +8,12 @@ function createSeaBattle () {
       },
       {
          id: '01',
-         x: { ship: false, shot: true },
+         x: { ship: false, shot: false },
          y: { ship: false, shot: false }
       },
       {
          id: '02',
-         x: { ship: false, shot: true },
+         x: { ship: false, shot: false },
          y: { ship: false, shot: false }
       },
       {
@@ -23,12 +23,12 @@ function createSeaBattle () {
       },
       {
          id: '04',
-         x: { ship: false, shot: true },
-         y: { ship: false, shot: true }
+         x: { ship: false, shot: false },
+         y: { ship: false, shot: false }
       },
       {
          id: '05',
-         x: { ship: false, shot: true },
+         x: { ship: false, shot: false },
          y: { ship: false, shot: false }
       },
       {
@@ -43,90 +43,86 @@ function createSeaBattle () {
       },
       {
          id: '08',
-         x: { ship: false, shot: true },
+         x: { ship: false, shot: false },
          y: { ship: false, shot: false }
       },
       {
          id: '09',
-         x: { ship: false, shot: true },
+         x: { ship: false, shot: false },
          y: { ship: false, shot: false }
       }
    ];
+
    return (a) => {
       return (b) => {
-         function shots () {
-            try {
-               if (b === '') {
-                  throw new Error('an exception Empty string');
-               }
-               if (isNaN(b)) {
-                  throw new Error('an exception Not a Number');
-               }
-               b = Number(b);
-               if (b < 0) {
-                  throw new Error('an exception less then 0');
-               }
-               if (b > 0) {
-                  throw new Error('an exception bigger then 0');
-               }
-            } catch (e) {
-               return e;
+         try {
+            if (b === '') {
+               throw new Error('an exception Empty string');
             }
-            try {
-               if (a === '') {
-                  throw new Error('an exception Empty string');
-               }
-               if (isNaN(a)) {
-                  throw new Error('an exception not a number');
-               }
-               a = Number(a);
-               if (a < 0) {
-                  throw new Error('an exception less then 0');
-               }
-               if (a > 9) {
-                  throw new Error('an exception bigger then 9');
-               }
-            } catch (e) {
-               return e;
+            if (isNaN(b)) {
+               throw new Error('an exception Not a Number');
             }
-            seaMap[a].x.shot = true;
-            seaMap[b].y.shot = true;
+            b = Number(b);
+            if (b < 0) {
+               throw new Error('an exception less then 0');
+            }
+            if (b > 0) {
+               throw new Error('an exception bigger then 0');
+            }
+            if (a === '') {
+               throw new Error('an exception Empty string');
+            }
+            if (isNaN(a)) {
+               throw new Error('an exception not a number');
+            }
+            a = Number(a);
+            if (a < 0) {
+               throw new Error('an exception less then 0');
+            }
+            if (a > 9) {
+               throw new Error('an exception bigger then 9');
+            }
+         } catch (e) {
+            return e + console.log(e);
+         }
+         seaMap[a].x.shot = true;
+         seaMap[b].y.shot = true;
+
+         function shotOver () {
+            seaMap.filter(gameOver);
+            function gameOver (value) {
+               return (value.x.shot !== true);
+            }
+         }
+         shotOver();
+         try {
+            if (shotOver.length === 0) {
+               throw new Error('an exception NO MORE SHOTS - Game Over');
+            }
+            if (seaMap[a].x.shot === true) {
+               throw new Error('an exception U shot allready');
+            }
+         } catch (e) {
+            return e;
          }
 
-         shots();
-         const shotOver = seaMap.filter(gameOver);
-         function gameOver (value) {
-            try {
-               if (shotOver.length === 0) {
-                  throw new Error('an exception NO MORE SHOTS - Game Over');
-               }
-               if (seaMap[a].x.shot === true) {
-                  throw new Error('an exception U shot allready');
-               }
-            } catch (e) {
-               return e;
+         if (a === 3) {
+            if (seaMap[3].x.shot === true) {
+               return 1;
             }
-            return (value.x.shot !== true);
+         } else if (a === 6 || a === 7) {
+            if (seaMap[6].x.shot === true && seaMap[7].x.shot === true) {
+               return 1;
+            } else if ((seaMap[6].x.shot === true && seaMap[7].x.shot === false) || (seaMap[6].x.shot === false && seaMap[7].x.shot === true)) {
+               return 1;
+            }
+         } else {
+            return -1;
          }
-         console.log(shotOver);
-
-         const answer = function shot () {
-            if (a === 3) {
-               if (seaMap[3].x.shot === true) {
-                  return 1;
-               }
-            } else if (a === 6 || a === 7) {
-               if (seaMap[6].x.shot === true && seaMap[7].x.shot === true) {
-                  return 1;
-               } else if ((seaMap[6].x.shot === true && seaMap[7].x.shot === false) || (seaMap[6].x.shot === false && seaMap[7].x.shot === true)) {
-                  return 1;
-               }
-            } else {
-               return -1;
-            }
-         };
-         return answer();
       };
    };
 }
+const seaBattle = createSeaBattle();
+seaBattle(3)(0);
+
 module.exports = createSeaBattle;
--- homework/yarm/index.spec.js
+++ homework/yarm/index.spec.js
@@ -2,4 +2,5 @@ createSeaBattle = require('./index.js');
 let seaBattle = createSeaBattle();
 test('kill 1deck ship', () => {
    expect(seaBattle(3)(0)).toBe(1);
+   expect(seaBattle(2)(0)).toBe(-1);
 });
