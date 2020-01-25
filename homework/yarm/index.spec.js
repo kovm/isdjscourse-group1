eda0f0edb5292510476a302abfb7f63d00dcf3a7
Yaroslav <4cast@ukr.net>
2020-01-25 20:36:05 +0200

    add some jest test

--- homework/yarm/index.spec.js
+++ homework/yarm/index.spec.js
@@ -2,6 +2,10 @@ createSeaBattle = require('./index.js');
 let seaBattle = createSeaBattle();
 test('kill 1deck ship', () => {
    expect(seaBattle(3)(0)).toBe(1);
+});
+test('U missed!!!', () => {
    expect(seaBattle(2)(0)).toBe(-1);
+});
+test('WRONG input data', () => {
    expect(seaBattle(22)(0)).toStrictEqual(Error('an exception bigger then 9'));
 });
